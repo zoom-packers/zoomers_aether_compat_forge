e2225409e4545305e91f474ebd908d58626b6c5f
qxZap <mileamihai@outlook.it>
2024-08-27 10:24:13 +0300

    first commit

---
+++ src/main/java/com/qxzap/zoomers_aether_compat_forge/ZoomersAetherCompatForge.java
@@ -0,0 +1,40 @@
+package com.qxzap.zoomers_aether_compat_forge;
+
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.server.ServerStartingEvent;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import com.mojang.logging.LogUtils;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.slf4j.Logger;
+
+@Mod(ZoomersAetherCompatForge.MODID)
+public class ZoomersAetherCompatForge {
+    public static final String MODID = "zoomers_aether_compat_forge";
+    private static final Logger LOGGER = LogUtils.getLogger();
+    public ZoomersAetherCompatForge() {
+        IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+    private void commonSetup(final FMLCommonSetupEvent event) {
+    }
+
+    @SubscribeEvent
+    public void onServerStarting(ServerStartingEvent event) {
+    }
+
+    @Mod.EventBusSubscriber(modid = MODID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
+    public static class ClientModEvents {
+
+        @SubscribeEvent
+        public static void onClientSetup(FMLClientSetupEvent event)
+        {
+
+        }
+    }
+}
---
+++ src/main/java/com/qxzap/zoomers_aether_compat_forge/mixin/ChestMimicRendererMixin.java
@@ -0,0 +1,40 @@
+package com.qxzap.zoomers_aether_compat_forge.mixin;
+
+import com.aetherteam.aether.blockentity.ChestMimicBlockEntity;
+import net.minecraft.world.level.block.state.properties.ChestType;
+import com.aetherteam.aether.client.renderer.blockentity.ChestMimicRenderer;
+import net.minecraft.client.renderer.Sheets;
+import net.minecraft.client.resources.model.Material;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraftforge.fml.ModList;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.*;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import noobanidus.mods.lootr.config.ConfigManager;
+
+@Mixin(ChestMimicRenderer.class)
+public class ChestMimicRendererMixin {
+
+    @Shadow
+    private boolean xmasTextures;
+
+    private static final Material OLD_LOOTR_MATERIAL = new Material(Sheets.CHEST_SHEET, new ResourceLocation("lootr", "old_chest"));
+
+    @Inject(method = "getMaterial", at = @At("HEAD"), remap = false)
+    private void injectCustomMaterial(ChestMimicBlockEntity blockEntity, CallbackInfoReturnable<Material> cir) {
+        if (ModList.get().isLoaded("lootr")) {
+            if (!ConfigManager.isVanillaTextures()) {
+                if (ConfigManager.isOldTextures()) {
+                    cir.setReturnValue(OLD_LOOTR_MATERIAL);
+                } else {
+                    cir.setReturnValue(new Material(Sheets.CHEST_SHEET, new ResourceLocation("lootr", "chest")));
+                }
+                return;
+            }
+        }
+        // Default behavior if no conditions match
+        cir.setReturnValue(Sheets.chooseMaterial(blockEntity, ChestType.SINGLE, this.xmasTextures));
+    }
+}
